998ac6ebe119d0a40877e7fb2a127887a14ca002
Abhiram302 <abhiramburra3@gmail.com>
2025-10-17 14:30:34 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,36 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [summ, setSum] = useState(0);
+  //   const [inputValue, setInputValue] = useState("");
+  const [numbers, setNumbers] = useState([]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const total = numbers.reduce((a, b) => a + b, 0);
+      setSum(total);
+    }, 0);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [numbers]);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Sum Calculator</h2>
+
+      <input
+        type="number"
+        onChange={(e) => {
+          //   setSum(summ + parseInt(e.target.value));
+          if (e.target.value !== "") {
+            setNumbers([...numbers, parseInt(e.target.value)]);
+          }
+        }}
+      ></input>
+      <p>sum: {summ}</p>
     </div>
-  )
+  );
 }
 
 export default App
